8cb2036879b5cb7bff6b6e2a7f9a411952faac81
spacelab2 <infosuemcmahon@gmail.com>
2016-05-15 16:15:16 +0100

    Update design

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+﻿var gsap = require('gsap');
+
+var circleOne = document.getElementsByClassName('circle-one');
+TweenMax.from(circleOne, 2, {
+    opacity: 0,
+    scale: 0,
+    ease: Bounce.easeOut
+});
+
+
+var lines = document.getElementsByClassName('line');
+TweenMax.staggerFrom(lines, 2, {
+    opacity: 0,
+    y: 200,
+    rotation: 360,
+    scale: 2,
+    delay: 2
+}, 0.2);
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+﻿/*jslint node, maxlen: 120 */
+
+"use strict";
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(minifyCss())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('public/js/bundle.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js/min'));
+});
+
+gulp.task('watch', ['sass', 'scripts'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('public/js/bundle.js', ['scripts']);
+});
+
