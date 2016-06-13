af73e0f7243d0c806545ebddca8a6ead86f41e27
spacelab2 <infosuemcmahon@gmail.com>
2016-06-13 22:42:30 +0100

    Added sceneOne, sceneTwo, sceneThree animation drafts

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,63 @@
-﻿var TweenMax = require('gsap');
+﻿/*jslint browser:true */
 
-console.log(TweenMax);
+var TweenMax = require("gsap");
+
+var layoutRight = document.getElementsByClassName("layout-right");
+var layoutLeft = document.getElementsByClassName("layout-left");
+var logo = document.getElementsByClassName("logo");
+var introText = document.getElementsByClassName("intro");
+var menu = document.querySelector(".menu");
+var close = document.querySelector(".menu-close");
+var overlay = document.getElementsByClassName("layout-overlay");
+
+var tlSceneOne = new TimelineMax();
+var tlSceneTwo = new TimelineMax();
+var tlSceneThree = new TimelineMax();
+
+function sceneOne() {
+    "use strict";
+    tlSceneOne
+        .set(layoutLeft, { x: '50%' })
+        .set(layoutRight, { x: '100%' })
+        .set(logo, { scale: 0.5, opacity: 0, rotationX: 45 })
+        .set(introText, { y: '300%', opacity: 0 })
+        .add('logo')
+        .to(logo, 1, { scale: 1, opacity: 1, rotationX: 360, ease: Power1.easeOut }, 'logo')
+        .add('layoutLeft')
+        .to(layoutLeft, 0.4, { x: '0%', ease: Power1.easeOut })
+        .add('layoutRight')
+        .to(layoutRight, 0.4, { x: '0%', ease: Power1.easeOut }, 'layoutLeft+=0.30')
+        .to(introText, 1, { y: '0%', opacity: 1 }, 'layoutRight')
+}
+
+function sceneTwo() {
+    "use strict";
+    tlSceneTwo
+        .set(overlay, { opacity: 0 })
+        .set(overlay, { css: { display: 'block' } })
+        .set(menu, { css: { display: 'none' } })
+        .set(close, {css: {display: 'block' }})
+        .add('overlay')
+        .to(overlay, 0.4, { opacity: 1 })
+        .to(layoutRight, 0.4, { opacity: 0 }, 'overlay')
+        .to(layoutLeft, 0.4, { opacity: 0 }, 'overlay')
+}
+
+function sceneThree() {
+    "use strict";
+    tlSceneThree
+        .set(overlay, { css: { display: 'none' } })
+        .set(menu, { css: { display: 'block' } })
+        .set(close, { css: { display: 'none' } })
+        .add('overlay')
+        .to(overlay, 0.4, { opacity: 0 })
+        .to(layoutRight, 0.4, { opacity: 1 }, 'overlay')
+        .to(layoutLeft, 0.4, { opacity: 1 }, 'overlay')
+}
+
+document.addEventListener("DOMContentLoaded", sceneOne, false);
+menu.addEventListener("click", sceneTwo, false);
+close.addEventListener("click", sceneThree, false);
+
 
 
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,7 +1,65 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var TweenMax = require('gsap');
+/*jslint browser:true */
 
-console.log(TweenMax);
+var TweenMax = require("gsap");
+
+var layoutRight = document.getElementsByClassName("layout-right");
+var layoutLeft = document.getElementsByClassName("layout-left");
+var logo = document.getElementsByClassName("logo");
+var introText = document.getElementsByClassName("intro");
+var menu = document.querySelector(".menu");
+var close = document.querySelector(".menu-close");
+var overlay = document.getElementsByClassName("layout-overlay");
+
+var tlSceneOne = new TimelineMax();
+var tlSceneTwo = new TimelineMax();
+var tlSceneThree = new TimelineMax();
+
+function sceneOne() {
+    "use strict";
+    tlSceneOne
+        .set(layoutLeft, { x: '50%' })
+        .set(layoutRight, { x: '100%' })
+        .set(logo, { scale: 0.5, opacity: 0, rotationX: 45 })
+        .set(introText, { y: '300%', opacity: 0 })
+        .add('logo')
+        .to(logo, 1, { scale: 1, opacity: 1, rotationX: 360, ease: Power1.easeOut }, 'logo')
+        .add('layoutLeft')
+        .to(layoutLeft, 0.4, { x: '0%', ease: Power1.easeOut })
+        .add('layoutRight')
+        .to(layoutRight, 0.4, { x: '0%', ease: Power1.easeOut }, 'layoutLeft+=0.30')
+        .to(introText, 1, { y: '0%', opacity: 1 }, 'layoutRight')
+}
+
+function sceneTwo() {
+    "use strict";
+    tlSceneTwo
+        .set(overlay, { opacity: 0 })
+        .set(overlay, { css: { display: 'block' } })
+        .set(menu, { css: { display: 'none' } })
+        .set(close, {css: {display: 'block' }})
+        .add('overlay')
+        .to(overlay, 0.4, { opacity: 1 })
+        .to(layoutRight, 0.4, { opacity: 0 }, 'overlay')
+        .to(layoutLeft, 0.4, { opacity: 0 }, 'overlay')
+}
+
+function sceneThree() {
+    "use strict";
+    tlSceneThree
+        .set(overlay, { css: { display: 'none' } })
+        .set(menu, { css: { display: 'block' } })
+        .set(close, { css: { display: 'none' } })
+        .add('overlay')
+        .to(overlay, 0.4, { opacity: 0 })
+        .to(layoutRight, 0.4, { opacity: 1 }, 'overlay')
+        .to(layoutLeft, 0.4, { opacity: 1 }, 'overlay')
+}
+
+document.addEventListener("DOMContentLoaded", sceneOne, false);
+menu.addEventListener("click", sceneTwo, false);
+close.addEventListener("click", sceneThree, false);
+
 
 
 
